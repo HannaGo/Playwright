fd131304137bd9be59729f98113bb3dd9a3ba920
Anna Goncharova <anna.goncharova@motorolasolutions.com>
2025-10-24 16:34:23 +0200

    reporting to html file

--- playwright-automation-framework/src/step-definitions/hooks/hooks.ts
+++ playwright-automation-framework/src/step-definitions/hooks/hooks.ts
@@ -1,4 +1,4 @@
-import {After, AfterAll, Before, BeforeAll} from '@cucumber/cucumber'; 
+import {After, AfterAll, Before, BeforeAll, Status} from '@cucumber/cucumber'; 
 import { Browser, chromium} from '@playwright/test';
 import{ pageFixture } from './browserContextFixture';
 
@@ -21,7 +21,21 @@ Before(async function() {
     
 });
 
-After(async function() {
+After(async function({ pickle, result }) {
+    if(result?.status === Status.FAILED){
+        if(pageFixture.page){
+            const screenshotPath = `./reports/screenshots/${pickle.name}-${Date.now()}.png`;
+            const image = await pageFixture.page.screenshot({ 
+                path: screenshotPath, 
+                type: 'png', 
+                //timeout: 60000
+            });
+            await this.attach(image, 'image/png');
+        } else{
+            console.error("PageFixture.page is underfined");
+        }
+    }
+   
     await pageFixture.page.close();
     await browser.close();
 });
