a2c732ae071b9d7580dab0c7f8cff57274a14d92
Anna Goncharova <anna.goncharova@motorolasolutions.com>
2025-10-25 16:54:01 +0200

    there is an error in hooks file, please ifx it next time. lesson 70.

--- playwright-automation-framework/src/step-definitions/hooks/hooks.ts
+++ playwright-automation-framework/src/step-definitions/hooks/hooks.ts
@@ -1,21 +1,39 @@
 import {After, AfterAll, Before, BeforeAll, Status} from '@cucumber/cucumber'; 
-import { Browser, chromium} from '@playwright/test';
+import { Browser, BrowserType, chromium, firefox, webkit} from '@playwright/test';
 import{ pageFixture } from './browserContextFixture';
 
 //load env variables from .env file
-import { config as laadEnv } from 'dotenv';
+import { config as loadEnv } from 'dotenv';
 const env = loadEnv({path: './env/.env'});
 
 
 //Create a configuratuon object for easy access to env variables
 const config = {
     headless: env.parsed?.HEADLESS === 'true',
-    broswer: env.parsed?.UI_AUTOMATION_BROWSER || 'chromium',
-    width: parsedInt(env.parsed?.BROWSER_WIDTH || '1920'),
-    height: parsedInt(env.parsed?.BROWSER_HEIGHT || '1080'),
+    browser: env.parsed?.UI_AUTOMATION_BROWSER || 'chromium',
+    width: parseInt(env.parsed?.BROWSER_WIDTH || '1920'),
+    height: parseInt(env.parsed?.BROWSER_HEIGHT || '1080'),
 }
 
-let browser: Browser;
+//Create dictionary mapping browser names to their launch functions
+const browsers: {[key: string]: BrowserType} = {
+    'chromium': chromium,
+    'firefox': firefox,
+    'webkit': webkit
+    // Add other browsers like firefox and webkit if needed. see playwright.config.ts for reference
+};
+
+let browserInstance: Browser | null = null;
+
+async function initializeBrowserContext(selectedBrowser: string): Promise<Browser> {
+    const launchBrowser = browsers[selectedBrowser];
+    if(!launchBrowser){
+        throw new Error(`Invalid browser selected: ${selectedBrowser}`);
+    }
+
+    return await launchBrowser.launch({headless: config.headless});
+};
+
 
 //Runs once Before all Scenarios
 BeforeAll(async function() {
@@ -27,6 +45,7 @@ AfterAll(async function() {
     console.log("\n Test suites execution completed. ");
 });
 
+// Before hook: Runs before each scenario
 Before(async function() {
         browser = await chromium.launch({ headless: false });
         pageFixture.context = await browser.newContext({ viewport: { width: 1920, height: 1080 } });
